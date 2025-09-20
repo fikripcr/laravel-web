21dfbf64235f60eceea9662ec613783ff7182111
Fikri Muhaffizh <fikrimuhaffizh@gmail.com>
2025-09-20 22:52:22 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+
+Route::get('/mahasiswa', [MahasiswaController::class,'index']);
+
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/mahasiswa/{param1?}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('/home',[HomeController::class,'index']);
