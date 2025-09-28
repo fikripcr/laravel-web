e4aa830effa95bcdd83355c43391fc24b6a27313
Fikri Muhaffizh <fikrimuhaffizh@gmail.com>
2025-09-28 21:56:03 +0700

    tambah menu about

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,6 +74,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
                 </ul>
             </div>
         </div>
