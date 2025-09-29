f8cee8fa4323b341ec4f0fcb171e56d9f146a5fb
Fikri Muhaffizh <fikrimuhaffizh@gmail.com>
2025-09-29 09:39:45 +0700

    Penambahan Question Controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
@@ -33,5 +34,8 @@ Route::get('/about', function () {
 });
 
 Route::get('/home',[HomeController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
 
 
